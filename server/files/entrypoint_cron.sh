ad37482cc493edf5e85adb93fe6d53fa254d21c7
Jason Kendall <coolacid@gmail.com>
2019-11-26 16:18:52 -0500

    Remove DCSO fake cron, and use real cron

--- server/files/entrypoint_cron.sh
+++ server/files/entrypoint_cron.sh
@@ -1,29 +1,17 @@
 #!/bin/sh
-set -e
 
-NC='\033[0m' # No Color
-Light_Green='\033[1;32m'  
-echo (){
-    command echo -e $1
-}
+cat << EOF > /etc/cron.d/misp
+00 0 * * * www-data /var/www/MISP/app/Console/cake Server pull "$CRON_USER_ID" >/dev/stdout 2>/dev/stdout
+10 0 * * * www-data /var/www/MISP/app/Console/cake Server push "$CRON_USER_ID" >/dev/stdout 2>/dev/stdout
+20 0 * * * www-data /var/www/MISP/app/Console/cake Server cacheFeed "$CRON_USER_ID" all >/dev/stdout 2>/dev/stdout
+30 0 * * * www-data /var/www/MISP/app/Console/cake Server fetchFeed "$CRON_USER_ID" all >/dev/stdout 2>/dev/stdout
 
-STARTMSG="${Light_Green}[ENTRYPOINT_CRON]${NC}"
+00 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateGalaxies >/dev/stdout 2>/dev/stdout
+10 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateTaxonomies >/dev/stdout 2>/dev/stdout
+20 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateWarningLists >/dev/stdout 2>/dev/stdout
+30 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateNoticeLists >/dev/stdout 2>/dev/stdout
+40 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateObjectTemplates >/dev/stdout 2>/dev/stdout
 
-# Wait until entrypoint apache is ready
-while (true)
-do
-    sleep 2
-    [ -f /entrypoint_apache.install ] && continue
-    break
-done
+EOF
 
-[ -n "$CRON_INTERVAL" ] && INTERVAL="$CRON_INTERVAL"
-( [ -z "$CRON_INTERVAL" ] || [ "$CRON_INTERVAL" = 0 ] ) && echo "$STARTMSG Deactivate cron job." && exit
-[ -z "$CRON_USER_ID" ] && USER_ID=1
-
-
-# wait for the first round
-echo "$STARTMSG Wait $INTERVAL seconds, then start the first intervall." && sleep "$INTERVAL" 
-# start cron job
-echo "$STARTMSG Start cron job" && misp_cron.sh "$INTERVAL" "$USER_ID"
-
+cron -f

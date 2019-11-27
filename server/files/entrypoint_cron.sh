3037facb26c3ef35e7191ad94d61eebc95a8d434
Jason Kendall <coolacid@gmail.com>
2019-11-27 09:09:20 -0500

    Show output of cron jobs to docker console

--- server/files/entrypoint_cron.sh
+++ server/files/entrypoint_cron.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+# Create the misp cron tab
 cat << EOF > /etc/cron.d/misp
-00 0 * * * www-data /var/www/MISP/app/Console/cake Server pull "$CRON_USER_ID" >/dev/stdout 2>/dev/stdout
-10 0 * * * www-data /var/www/MISP/app/Console/cake Server push "$CRON_USER_ID" >/dev/stdout 2>/dev/stdout
-20 0 * * * www-data /var/www/MISP/app/Console/cake Server cacheFeed "$CRON_USER_ID" all >/dev/stdout 2>/dev/stdout
-30 0 * * * www-data /var/www/MISP/app/Console/cake Server fetchFeed "$CRON_USER_ID" all >/dev/stdout 2>/dev/stdout
+00 0 * * * www-data /var/www/MISP/app/Console/cake Server pull "$CRON_USER_ID" >/tmp/cronlog 2>/tmp/cronlog
+10 0 * * * www-data /var/www/MISP/app/Console/cake Server push "$CRON_USER_ID" >/tmp/cronlog 2>/tmp/cronlog
+20 0 * * * www-data /var/www/MISP/app/Console/cake Server cacheFeed "$CRON_USER_ID" all >/tmp/cronlog 2>/tmp/cronlog
+30 0 * * * www-data /var/www/MISP/app/Console/cake Server fetchFeed "$CRON_USER_ID" all >/tmp/cronlog 2>/tmp/cronlog
 
-00 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateGalaxies >/dev/stdout 2>/dev/stdout
-10 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateTaxonomies >/dev/stdout 2>/dev/stdout
-20 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateWarningLists >/dev/stdout 2>/dev/stdout
-30 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateNoticeLists >/dev/stdout 2>/dev/stdout
-40 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateObjectTemplates >/dev/stdout 2>/dev/stdout
+00 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateGalaxies >/tmp/cronlog 2>/tmp/cronlog
+10 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateTaxonomies >/tmp/cronlog 2>/tmp/cronlog
+20 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateWarningLists >/tmp/cronlog 2>/tmp/cronlog
+30 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateNoticeLists >/tmp/cronlog 2>/tmp/cronlog
+45 1 * * * www-data /var/www/MISP/app/Console/cake Admin updateObjectTemplates >/tmp/cronlog 2>/tmp/cronlog
 
 EOF
 
-cron -f
+# Build a fifo buffer for the cron logs, 777 so anyone can write to it
+mkfifo /tmp/cronlog
+chmod 777 /tmp/cronlog
+
+cron -f | tail -f /tmp/cronlog

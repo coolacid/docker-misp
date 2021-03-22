8a1073c641a6bcdcab8a4d729d79e1523f44bbf5
Garrit Schroeder <garritsidney.schroeder@ottogroup.com>
2021-03-22 18:29:02 +0100

    persist misp sessions in redis db

--- server/files/entrypoint_fpm.sh
+++ server/files/entrypoint_fpm.sh
@@ -2,12 +2,14 @@
 
 change_php_vars(){
     for FILE in /etc/php/*/fpm/php.ini
-    do  
+    do
         [[ -e $FILE ]] || break
         sed -i "s/memory_limit = .*/memory_limit = 2048M/" "$FILE"
         sed -i "s/max_execution_time = .*/max_execution_time = 300/" "$FILE"
         sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" "$FILE"
         sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
+        sed -i "s/session.save_handler = .*/session.save_handler = redis/" "$FILE"
+        sed -i "s/;session.save_path = .*/session.save_path = \"tcp:\/\/$REDIS_FQDN:6379\"/" "$FILE"
     done
 }
 
